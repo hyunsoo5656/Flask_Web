ee0b4659be211bb09806eca30059aad378389c2c
name <hyunsoo5656@gmail.com>
2020-07-20 16:08:14 +0900

    navbar

--- app.py
+++ app.py
@@ -3,13 +3,32 @@ from flask import Flask ,render_template
 app = Flask(__name__)
 app.debug=True
 
-@app.route('/data')
+@app.route('/')
 def index():
     #return 'Hello World!!'
 
     print("Success")
     #return "test"
-    return render_template('home.html')
+    return render_template('home.html', hello="Garykim")
+
+@app.route('/about')
+def aubout():
+    #return 'Hello World!!'
+
+    print("Success")
+    #return "test"
+    return render_template('about.html', hello="Garykim")
+
+@app.route('/articles')
+
+def articles():
+    #return 'Hello World!!'
+
+    print("Success")
+    #return "test"
+    return render_template('articles.html', hello="Garykim")
+
+
 
 if __name__== '__main__':
     # app.run(host= '0.0.0.0', port = '8080')
